fa5cc1c33f4e48c1898cb077c0d78e38ac998187
richardyoungdev <richardyoungdev@gmail.com>
2023-01-11 11:24:12 -0500

    product api routers setup

---
+++ server/api/products.js
@@ -0,0 +1,37 @@
+const router = require("express").Router();
+const Product = require("../db/models/Product");
+
+// GET /api/products/
+router.get("/", async (req, res, next) => {
+    try {
+        const products = await Product.findAll();
+        res.send(products);
+    } catch(error){
+        next(error)
+    }
+});
+
+// GET /api/products/:productId
+router.get("/:productId", async (req, res, next) => {
+    try{
+        const product = await Product.findByPk(req.params.productId);
+        res.send(product);
+    } catch(error){
+        next(error)
+    }
+});
+
+// GET /api/products/:category
+router.get("/:category", async (req, res, next) => {
+    try{
+        const products = await Product.findAll({
+            where: {
+                category: req.params.category,
+            }
+        });
+    } catch(error){
+        next(error)
+    }
+});
+
+module.exports = router
