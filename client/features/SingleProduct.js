ee1a6d4a5ea45562787939347d532cb334831d05
wragyu <rfgalang196@gmail.com>
2023-01-12 11:37:41 -0500

    Added SingleProduct Component temp

---
+++ client/features/SingleProduct.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const SingleProduct = () => {
+
+  return (
+    <div id="single-product-page">
+      <h1>Single Product Name</h1>
+      <img/>
+      <p>Description</p>
+      <h3>$$$$</h3>
+      <button>Add to Cart</button>
+    </div>
+  );
+
+}
+
+export default SingleProduct;
