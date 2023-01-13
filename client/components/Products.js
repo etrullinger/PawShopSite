da3ae4dd3d504f84cb0af209148fd5ccec0225d6
richardyoungdev <richardyoungdev@gmail.com>
2023-01-13 11:40:32 -0500

    Dropdown progress: mapped over fake data

--- client/components/Products.js
+++ client/components/Products.js
@@ -11,6 +11,7 @@ const data = [
     {id: 1, name: "Milk - 1%", category: "food", description: "Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.", price: 26.22, imageUrl: "https://i.imgur.com/fVPMvho.jpg"},
     {id: 2, name: "Pup-eroni", category: "treats", description: "Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.\n\nInteger ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.\n\nNam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.", price: 90.04, imageUrl: "https://i.imgur.com/s43kjIJ.jpg"},
     {id: 3, name: "Collar", category: "supplies", description: "Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.\n\nFusce consequat. Nulla nisl. Nunc nisl.", price: 62.96, imageUrl: "https://i.imgur.com/ZxyrGfO.jpg"},
+    {id: 3, name: "Collar", category: "Cat Supplies", description: "Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.\n\nFusce consequat. Nulla nisl. Nunc nisl.", price: 62.96, imageUrl: "https://i.imgur.com/ZxyrGfO.jpg"}
 ]
 
 // Write a component to display a list of all products (at least their name, category, price, short description, and a add to cart button)
@@ -34,8 +35,8 @@ const Products = () => {
             <label>Category</label>
             <select>
                 {
-                types.map((type) => (
-                    <option value={type}>{type}</option>
+                data.map((product) => (
+                    <option value={product.category}>{product.category}</option>
                 ))
                 }
             </select>
