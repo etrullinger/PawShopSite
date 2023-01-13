f285e65d798e1fb8a69848af8268a73c12292b17
richardyoungdev <richardyoungdev@gmail.com>
2023-01-13 16:33:50 -0500

    Added 30 dog data

--- client/components/Products.js
+++ client/components/Products.js
@@ -3,43 +3,23 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { selectProducts } from '../features/productsSlice'
 
-// hardcoded data to test categories
-// dropdown categories 
-// const types = ["all", "food", "treats", "supplies"]
-// modified seed data
-// const data = [
-//     {id: 1, name: "Milk - 1%", category: "food", description: "Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.", price: 26.22, imageUrl: "https://i.imgur.com/fVPMvho.jpg"},
-//     {id: 2, name: "Pup-eroni", category: "treats", description: "Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.\n\nInteger ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.\n\nNam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.", price: 90.04, imageUrl: "https://i.imgur.com/s43kjIJ.jpg"},
-//     {id: 3, name: "Collar", category: "supplies", description: "Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.\n\nFusce consequat. Nulla nisl. Nunc nisl.", price: 62.96, imageUrl: "https://i.imgur.com/ZxyrGfO.jpg"},
-//     {id: 4, name: "Collar", category: "supplies", description: "Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.\n\nFusce consequat. Nulla nisl. Nunc nisl.", price: 62.96, imageUrl: "https://i.imgur.com/ZxyrGfO.jpg"}
-// ]
-
 // Write a component to display a list of all products (at least their name, category, price, short description, and a add to cart button)
 const Products = () => {
     // store currently selected category
     const [category, setCategory] = useState("");
     const products = useSelector(selectProducts)
-    // console.log("products-->", products)
 
-    // Memoized results. Will re-evaluate any time selected  
-    // category changes  
-
-    // how to render all products when category is not selected? 
-
-
+    // Memoized results. Re-evaluates any time selected.
+    // category changes.
     const filteredData = useMemo(()=>{
         if(!category || category === "all") {
-            console.log("products from filteredData-->", products)
             return products;
         }
 
-        // console.log("data from filteredData-->", data)
         return products.filter(element => element.category === category)
     }, [category, products]);
 
     let uniqueCategories = [...new Set(products.map((item) => item.category))];
-
-    console.log("uniqueCategories-->", uniqueCategories)
 
     return (
     <div>
@@ -54,7 +34,6 @@ const Products = () => {
                 }
             </select>
         </form>
-
 
         {filteredData.map((product) =>(
             <div key={product.id} className="product" >
