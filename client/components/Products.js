8aadffa5117d0cdbe87024e902073b3fae8f1544
richardyoungdev <richardyoungdev@gmail.com>
2023-01-16 21:41:05 -0500

    Search filter works

--- client/components/Products.js
+++ client/components/Products.js
@@ -1,39 +1,90 @@
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { selectProducts } from '../features/productsSlice'
 import Button from '@mui/material/Button'
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import { FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search';
 
 // Write a component to display a list of all products (at least their name, category, price, short description, and a add to cart button)
 const Products = () => {
+    // store currently selected category
+    const [category, setCategory] = useState("");
+
+    // store search results
+    const [searchResults, setSearchResults] = useState([]);  
+    console.log("searchResults from outside of any function-->", searchResults)
+    console.log("typeof searchResults-->", typeof searchResults)
+    console.log("searchResults.length-->", searchResults.length)
+
     const products = useSelector(selectProducts)
 
-    const [productsData, setProductsData] = useState([]);
-    const [productSearchData, setProductSearchData] = useState([]);
-    const [name, setName] = useState('');
+    const handleSearch = (e) => {
+        console.log("search button clicked!")
+        // e.preventDefault();
+        const results = products.filter(product => product.name.toLowerCase().includes(searchResults.toLowerCase()));
+        console.log("results from handleSearch-->", results)
+        setSearchResults(results);
+    }
 
-    React.useEffect(()=>{
-        setProductsData(products)
-        setProductSearchData(products)
-    }, [name, products])
+    // Memoized results. Re-evaluates any time selected.
+    // category changes.
+    const filteredData = useMemo(()=>{
+        // why does the order of these two conditionals matter?
+        if(typeof searchResults === "object" && searchResults.length > 0) {
+            console.log("searchResults from filteredData-->", searchResults)
+            return searchResults
+        }
 
-    const handleSearch = () => {
-        console.log("search button clicked")
-        const newData = productsData.filter((product) => product.name.toLowerCase().includes(name.toLowerCase()))
-        setProductSearchData(newData)
-    };
+        if(!category || category === "all") {
+            return products;
+        } 
+        
+        return products.filter(element => element.category === category)
+    }, [category, products, searchResults]);
+
+    let uniqueCategories = [...new Set(products.map((item) => item.category))];
 
     return (
         <div>
-            <div>
-                <TextField type='text' placeholder='Search product...' onChange={(e)=> setName(e.target.value) }/>
-                <Button variant='contained' onClick={()=> handleSearch()}>Search</Button>
+            <div className='searchAndCategoryFilter'>
+
+                <div className='search-function'>
+                    <TextField 
+                        fullWidth 
+                        type="text" 
+                        placeholder="Search for product..." 
+                        onChange={(e) => setSearchResults(e.target.value)}
+                    />
+                    <Button onClick={ () => handleSearch() } variant="contained">Search</Button>
+                </div> 
+                
+                <br/>
+
+                <div className='category-filter'>
+                    <FormControl fullWidth>
+                        <InputLabel>Category</InputLabel>
+                        <Select 
+                            onChange={(e) => setCategory(e.target.value)}
+                            value={category}
+                            label="Category"
+                        >
+                            <MenuItem value="all" onChange={(e) => setCategory(e.target.value)}>All</MenuItem>
+                            {
+                                uniqueCategories.map((category) => (
+                                    <MenuItem onChange={(e) => setCategory(e.target.value)} key={category} value={category}>{category}</MenuItem>
+                                ))
+                            }
+                        </Select>
+                    </FormControl>
+                </div>
             </div>
+            
+
             <div className='productListingGrid_overlayWrapper'>
                 <div className='product-listings_gridContainer'>
-                    {productSearchData.map((product) =>(
+                    { filteredData.map((product) =>(
                         <div key={product.id} className='product-card'>
                             <div className='product-card_canvas'>
                                 <div className='product-card-image'>
@@ -60,6 +111,7 @@ const Products = () => {
                     }
                 </div>
             </div>
+
         </div>
     )
 }
