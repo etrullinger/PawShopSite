001281a2456e95b76e0542d9ef16a59ca78b45f6
wragyu <rfgalang196@gmail.com>
2023-01-18 10:22:49 -0500

    Progress on guestCart. Pulling from main

--- client/components/GuestCart.js
+++ client/components/GuestCart.js
@@ -4,37 +4,59 @@ import { Link } from 'react-router-dom';
 import { fetchCartAsync, selectCart } from '../features/cartSlice';
 import { selectProducts } from '../features/productsSlice';
 
-const cartFromLocalStorage = JSON.parse(localStorage.getItem('cart') || "[]")
+const inCart = [{
+  quantity: 1,
+  productId: 10,
+  userId: null,
+  }, {
+  quantity: 2,
+  productId: 2,
+  userId: null,
+  },
+]
 
 const GuestCart = () => {
-
-  const [ cart, setCart ] = useState(cartFromLocalStorage)
-
-  // array of guest cart products [{quantity, productId, userId}]
+  // array of user cart products [{quantity, productId, userId}]
   // const cart = useSelector(selectCart);
   // console.log("cart:", cart);
-
   const allProducts = useSelector(selectProducts);
-
   const dispatch = useDispatch();
+  const [ products, setProducts ] = useState(allProducts);
+
+  const [ cart, setCart ] = useState([]);
+
+  let localCart = localStorage.getitem("cart")
+
+  const addItem = (item)  =>   {}
+  const updateItem = (itemID, amount) => {}
+  const removeItem = (itemID) => {}
 
   useEffect(() => {
-    localStorage.setItem('cart', JSON.stringify(cart))
-  }, [cart]);
+    localCarrt = JSON.parse(localCart);
+    if (localcart){
+      setCart(localCart);
+    }
+  }, []);
 
   // useEffect(() => {
   //   dispatch(fetchCartAsync(props.userId));
-  // }, [dispatch]);
+  // }, [cart, dispatch]);
+
+  const handleRemove = (productToRemove) => {
+    console.log('remove button clicked')
+    setCart(cart.filter((product) => product.productId !== productToRemove.productId))
+    console.log("after remove click", cart)
+  }
 
   return (
     <div>
       <h3>Guest Shopping Cart</h3>
 
       {cart && cart.length ? cart.map((cartProduct) =>
-        <div key={`cart${props.userId}: product #${cartProduct.productId}`}>
+        <div key={cartProduct.productId}>
           <div>{cartProduct.quantity}</div>
 
-          {allProducts.filter((product) => product.id === cartProduct.productId).map((product) =>
+          {products.filter((product) => product.id === cartProduct.productId).map((product) =>
           <div key={`cartProductDetails #${product.id}`}>
             <p>{product.name}</p>
             <p>{product.price}</p>
@@ -42,10 +64,12 @@ const GuestCart = () => {
             <Link to={`/products/${product.id}`}>
               <img alt={product.name}  src={product.imageUrl} sx={{ width: 150, height: 150 }}/>
             </Link>
+
+            <button onClick={() => handleRemove(product)}>Remove from Cart</button>
           </div>)}
 
         </div>
-      ) : "No Items in Cart"}
+      ) : null}
 
     </div>
   )
